2afc7a55bb761610bed08ce7e054cfd018220053
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-09-22 21:24:31 -0300

    Migrando para o Reactive Forms - Parte 2

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2'
+import { msg } from 'src/app/shared/utils/msg';
 
 @Component({
   selector: 'app-register',
@@ -10,8 +11,6 @@ import Swal from 'sweetalert2'
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) { }
-
   registerForm: FormGroup = this.fb.group({
     role:['', [Validators.required]],
     fullName:['', [Validators.required]],
@@ -19,6 +18,11 @@ export class RegisterComponent implements OnInit {
     email:['',[Validators.required,Validators.email]],
     password:['',[Validators.required]]
   });
+
+  msg = msg;
+  constructor(private fb: FormBuilder) { }
+
+  
 
   ngOnInit(): void {
   }
@@ -38,57 +42,79 @@ export class RegisterComponent implements OnInit {
   }
 
   cadastrar(){
-    //Se entrou aqui, é porque o form esta valido!
 
-    //checa se alguma role foi checada
-    if(this.checkIfAnyRoleIsChecked() === false){
-      //alert('Você precisa marcar uma role!')
-        Swal.fire(
-            'Erro!',
-            'Você precisa marcar uma role!',
-            'error'
-          )
-        return; //quebra a função
+    if(this.registerForm.valid){
+      //(payload)
+      console.log(this.registerForm.value);
+    }else{
+      this.registerForm.markAllAsTouched();
     }
+    
+    // //Se entrou aqui, é porque o form esta valido!
 
-    //pegar dados do form - inicia a massa de dados
-    let payLoad = {
-        role:document.getElementsByName("role")[0].checked === true ? 'dev' : 'cliente',
-        fullName: document.querySelector("#fullName").value,
-        birthDate: document.querySelector("#birthDate").value,
-        email: document.querySelector("#email").value,
-        password: document.querySelector("#password").value
-    };
+    // //checa se alguma role foi checada
+    // if(this.checkIfAnyRoleIsChecked() === false){
+    //   //alert('Você precisa marcar uma role!')
+    //     Swal.fire(
+    //         'Erro!',
+    //         'Você precisa marcar uma role!',
+    //         'error'
+    //       )
+    //     return; //quebra a função
+    // }
 
-    //Enviar para a API
+    // //pegar dados do form - inicia a massa de dados
+    // let payLoad = {
+    //     role:document.getElementsByName("role")[0].checked === true ? 'dev' : 'cliente',
+    //     fullName: document.querySelector("#fullName").value,
+    //     birthDate: document.querySelector("#birthDate").value,
+    //     email: document.querySelector("#email").value,
+    //     password: document.querySelector("#password").value
+    // };
 
-    fetch("https://63177ac4ece2736550b47a15.mockapi.io/api/users", {
-        method: 'POST',
-        body: JSON.stringify(payLoad),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-    }).then(response => response.json())
-      .then(response =>{
+    // //Enviar para a API
 
-        //alert('Cadastrado com Sucesso!')
-        Swal.fire(
-            'Sucesso!',
-            'Cadastrado com Sucesso!',
-            'success'
-          );
+    // fetch("https://63177ac4ece2736550b47a15.mockapi.io/api/users", {
+    //     method: 'POST',
+    //     body: JSON.stringify(payLoad),
+    //     headers:{
+    //         'Content-Type': 'application/json'
+    //     }
+    // }).then(response => response.json())
+    //   .then(response =>{
+
+    //     //alert('Cadastrado com Sucesso!')
+    //     Swal.fire(
+    //         'Sucesso!',
+    //         'Cadastrado com Sucesso!',
+    //         'success'
+    //       );
         
-        localStorage.setItem("userName", response.fullName);
-        localStorage.setItem("role", response.role === "dev"?"Desenvolvedor":"Cliente");
-        localStorage.setItem("idClient",response.id)
+    //     localStorage.setItem("userName", response.fullName);
+    //     localStorage.setItem("role", response.role === "dev"?"Desenvolvedor":"Cliente");
+    //     localStorage.setItem("idClient",response.id)
 
-        //redirect para Listagem
-        window.location.href = "list.html";
-      })
-      .catch(error =>{
-        alert('Erro no servidor!');
-      });
+    //     //redirect para Listagem
+    //     window.location.href = "list.html";
+    //   })
+    //   .catch(error =>{
+    //     alert('Erro no servidor!');
+    //   });
 
-}
+  }
+
+  togleRole(role: 'dev' | 'cliente'){
+    this.registerForm.get('role')?.setValue(role);
+  }
+
+  isInvalid(inputName:string, validatorName:string):boolean{
+    const formControl:any = this.registerForm.get(inputName);
+
+    if(formControl.errors !== null){
+      return formControl.errors[validatorName] && formControl.touched;
+    }
+  }
+
+  
 
 }
---
+++ src/app/shared/utils/msg.ts
@@ -0,0 +1,5 @@
+export const msg ={
+    "required": "O campo é obrigatorio",
+    "email":"mail inválido",
+    "role": "Por favor, marque uma role acima"
+}
