cded5e9e37300d28df7075d34dd4d5adb5891ab6
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-09-28 21:42:08 -0300

    Melhorando a formatacao com pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http'
+import {LOCALE_ID, DEFAULT_CURRENCY_CODE} from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localePt, 'pt');
 @NgModule({
   declarations: [
     AppComponent
@@ -15,7 +20,17 @@ import {HttpClientModule} from '@angular/common/http'
     HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+        provide: LOCALE_ID,
+        useValue: 'pt'
+    },
+    {
+        provide:  DEFAULT_CURRENCY_CODE,
+        useValue: 'BRL'
+    },
+]
+,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/list/list.module.ts
+++ src/app/pages/list/list.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list.component';
 import { ListRoutingModule } from './list-routing.module';
 import { DfWrapperModule } from 'src/app/features/df-wrapper/df-wrapper.module';
+import { DfButtonModule } from 'src/app/shared/components/df-button/df-button.module';
 
 
 
@@ -13,7 +14,8 @@ import { DfWrapperModule } from 'src/app/features/df-wrapper/df-wrapper.module';
   imports: [
     CommonModule,
     ListRoutingModule,
-    DfWrapperModule
+    DfWrapperModule,
+    DfButtonModule
   ]
 })
 export class ListModule { }
